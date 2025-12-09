ba0adce7a224d1fdc65bae4c9d86c3ca3e681b74
Md Sayed Uddin <sayedrayhan10@gmail.com>
2025-12-09 23:55:33 +0600

    Make home page dynamic

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category, BlogPost
+
+
+# For BlogPost Slug field prepopulation
+class BlogPostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ('title', 'category', 'author', 'status', 'is_featured', 'updated_at',)
+    search_fields = ('title', 'category__name', 'author__username', 'status',)
+    list_editable = ('is_featured',)
 
 admin.site.register(Category)
+admin.site.register(BlogPost, BlogPostAdmin)
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.views import View
-
+from .models import Category, BlogPost
 
 class HomeView(View):
     def get(self, request):
-        return render(request, 'home.html')
+
+        categoriesQ = Category.objects.all()
+        featured_postsQ = BlogPost.objects.filter(is_featured=True, status='published').order_by('-updated_at')
+        posts = BlogPost.objects.filter(is_featured=False, status='published').order_by('-updated_at')
+        context = {
+            'categories': categoriesQ if categoriesQ.exists() else None,
+            'featured_posts': featured_postsQ if featured_postsQ.exists() else None,
+            'posts': posts if posts.exists() else None,
+        }
+
+        return render(request, 'home.html', context)
