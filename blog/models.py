ac19e36407a470018eb403072c0587b00441b8ee
Sayed <sayedrayhan10@gmail.com>
2025-12-09 14:57:40 +0600

    Add BlogPost Model

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
@@ -10,3 +11,21 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='posts')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    featured_image = models.ImageField(upload_to='blog_images/%Y/%m%d', blank=True, null=True)
+    short_description = models.TextField(max_length=500)
+    blog_body = models.TextField()
+    status = models.CharField(max_length=10, choices=[('draft', 'Draft'), ('published', 'Published')], default='draft')
+    is_featured = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
