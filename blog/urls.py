505d7dd20f4a761bbe82d3497fc32384f004f500
Sayed <sayedrayhan10@gmail.com>
2025-12-10 14:08:55 +0600

    Add base and category template and update home page

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, CategoryView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('category/<int:category_id>/', CategoryView.as_view(), name='category_view')
 ]
--- blog/views.py
+++ blog/views.py
@@ -9,9 +9,22 @@ class HomeView(View):
         featured_postsQ = BlogPost.objects.filter(is_featured=True, status='published').order_by('-updated_at')
         posts = BlogPost.objects.filter(is_featured=False, status='published').order_by('-updated_at')
         context = {
-            'categories': categoriesQ if categoriesQ.exists() else None,
-            'featured_posts': featured_postsQ if featured_postsQ.exists() else None,
-            'posts': posts if posts.exists() else None,
+            'categories': categoriesQ,
+            'featured_posts': featured_postsQ,
+            'posts': posts,
         }
 
         return render(request, 'home.html', context)
+    
+
+class CategoryView(View):
+    def get(self, request, category_id):
+        postsQ = BlogPost.objects.filter(status='published', category=category_id)
+        category_id = Category.objects.get(id=category_id)
+        categoriesQ = Category.objects.all()
+        context = {
+            'posts': postsQ,
+            'category': category_id,
+            'categories': categoriesQ,
+        }
+        return render(request, 'category.html', context)
