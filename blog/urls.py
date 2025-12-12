093897547d69ad21dedbc60bb284a7789a24594e
Md Sayed Uddin <sayedrayhan10@gmail.com>
2025-12-12 17:19:40 +0600

    Add search option

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HomeView, CategoryView, BlogPostDetailView
+from .views import HomeView, CategoryView, BlogPostDetailView, SearchView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('category/<int:category_id>/', CategoryView.as_view(), name='category_view'),
     path('blog/<slug:slug>/', BlogPostDetailView.as_view(), name='blogpost_detail'),
+    path('search/', SearchView.as_view(), name='search'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 from .models import Category, BlogPost, About, FollowUs
-
+from django.db.models import Q
 class HomeView(View):
     def get(self, request):
         # I have created a context processor to get all categories
@@ -54,3 +54,19 @@ class BlogPostDetailView(View):
             'post': post,
         }
         return render(request, 'blogpost_detail.html', context)
+    
+class SearchView(View):
+    def get(self, request):
+        query = request.GET.get('keyword', '')
+        postsQ = BlogPost.objects.filter(
+            Q(title__icontains=query) |
+            Q(short_description__icontains=query) |
+            Q(blog_body__icontains=query),
+            status='published'
+         ).order_by('-updated_at')
+
+        context = {
+            'posts': postsQ,
+            'search_query': query,
+        }
+        return render(request, 'search_results.html', context)
