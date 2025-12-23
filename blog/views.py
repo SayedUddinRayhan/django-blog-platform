fc9a91373117e636775306bd9ed6b3ac42ff4cb1
Md Sayed Uddin <sayedrayhan10@gmail.com>
2025-12-23 23:20:49 +0600

    Update search view and add paginator in the search result template

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 from .models import Category, BlogPost, About, FollowUs
 from django.db.models import Q
+from django.core.paginator import Paginator
 class HomeView(View):
     def get(self, request):
         # I have created a context processor to get all categories
@@ -58,15 +59,24 @@ class BlogPostDetailView(View):
 class SearchView(View):
     def get(self, request):
         query = request.GET.get('keyword', '')
-        postsQ = BlogPost.objects.filter(
+        postsQ = BlogPost.objects.none()
+
+        if query:
+            postsQ = BlogPost.objects.filter(
             Q(title__icontains=query) |
             Q(short_description__icontains=query) |
             Q(blog_body__icontains=query),
             status='published'
          ).order_by('-updated_at')
+            
+        
+        # Pagination
+        paginator = Paginator(postsQ, 3)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
 
         context = {
-            'posts': postsQ,
             'search_query': query,
+            'page_obj': page_obj
         }
         return render(request, 'search_results.html', context)
