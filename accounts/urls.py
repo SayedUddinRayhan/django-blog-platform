ac2bb1490d61881f1225eee46e843e6054e1d9ee
Sayed <sayedrayhan10@gmail.com>
2026-01-01 15:00:19 +0600

    Implement forgot password feature using Django Built-in Password Reset views and reset email verification using sendgrid

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import RegisterView, LoginView, LogoutView, ProfileUpdateView, CustomPasswordChangeView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
@@ -7,4 +8,10 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('profile/', ProfileUpdateView.as_view(), name='profile'),
     path('password_change/', CustomPasswordChangeView.as_view(), name='password_change'),
+
+    # Password reset (forgot password)
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='accounts/password_reset.html'), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'), name='password_reset_complete'),
 ]
